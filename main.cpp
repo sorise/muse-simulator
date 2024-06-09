0b3048c9df2d46df25325b0f06a4b8321b0574e2
cicer <jxkicker@163.com>
2024-06-09 12:39:15 +0800

    update

--- main.cpp
+++ main.cpp
@@ -14,8 +14,8 @@
 #include "simulator/encryption/ecc_secp256k1.hpp"
 #include "simulator/encryption/bitcoin_secp256k1.hpp"
 
-//#include "mcl/bn_c384_256.h"
-//#include "bls/bls.h"
+#include "mcl/bn_c384_256.h"
+#include "bls/bls.h"
 
 int main(int argc, char *argv[]) {
     MUSE_CPU_PROCESSING_MATRIX::get_ptr()->initial(20, 40);
